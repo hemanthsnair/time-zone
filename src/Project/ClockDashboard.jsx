a630e69c846e43335cf066a57399ac2bf8df358e
hemanthsnair <hsnair535@gmail.com>
2025-04-03 15:29:13 +0530

    updated

--- src/Project/ClockDashboard.jsx
+++ src/Project/ClockDashboard.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CityTime from './CityTime'
 
 export default function ClockDashboard() {
-    const cities = [
-        {name: 'Delhi', timeZone: 'Asia/Kolkata'},
-        {name: 'London', timeZone: 'Europe/London'} , 
-        {name: 'Frankfurt', timeZone: 'Europe/Berlin'} , 
-        {name: 'Tokyo', timeZone: 'Asia/Tokyo'}            
-    ]
+    const [cities,setCities] = useState([])
+    const defaultCities = [
+        { name: "Delhi", timeZone: "Asia/Kolkata" },
+    ];
+    useEffect(()=>{
+        const storedCities=JSON.parse(localStorage.getItem('cities'))||defaultCities;
+        console.log("Loaded Cities from Storage:", storedCities);
+        setCities(storedCities)
+    },[]);
+    useEffect(()=>{
+        localStorage.setItem('cities',JSON.stringify(cities))
+    },[cities]);
+
+    const addCity=()=>{
+        const timeZone = prompt("Enter Time Zone");
+        if(timeZone) {
+            setCities([...cities, {name: timeZone,timeZone}])
+        }
+    }
+
+    const deleteCity=(timeZone)=>{
+        const updatedCities= cities.filter((city)=>city.timeZone!=timeZone)
+        setCities(updatedCities)
+    }
   return (
     <div className='world-clock'>
         <h1>ClockDashboard</h1>
         <ul className='cities'>
-            {cities.map((city,index)=>(
-        <CityTime city={city} key={index}/>
-    )) }
+        <CityTime city={{name:"Local Time", timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone}}/>
+        <button onClick={addCity}>Add Time Zone</button>
+        {cities.map((city,index)=>(
+           < div key={index}>
+            <CityTime city={city}/>
+            <button onClick={()=>deleteCity(city.timeZone)}>Delete</button>
+           </div>
+        ))}
         </ul>
     </div>
   );
