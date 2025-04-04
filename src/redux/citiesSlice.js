e956d366264868f21743bcdaf3b40d0092f8b395
hemanthsnair <hsnair535@gmail.com>
2025-04-04 20:51:09 +0530

    committed

---
+++ src/redux/citiesSlice.js
@@ -0,0 +1,24 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+    cities: JSON.parse(localStorage.getItem("cities")) || [{ name: "Delhi", timeZone: "Asia/Kolkata" }],
+    timeZones: Intl.supportedValuesOf("timeZone"),
+  };
+
+  const citiesSlice = createSlice({
+    name: "cities",
+    initialState,
+    reducers: {
+      addCity: (state, action) => {
+        state.cities.push(action.payload);
+        localStorage.setItem("cities", JSON.stringify(state.cities));
+      },
+      removeCity: (state, action) => {
+        state.cities = state.cities.filter((city) => city.timeZone !== action.payload);
+        localStorage.setItem("cities", JSON.stringify(state.cities));
+      },
+    },
+  });
+  
+  export const { addCity, removeCity } = citiesSlice.actions;
+  export default citiesSlice.reducer;
